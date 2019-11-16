437a4d5f82d34be0b1b1eb0a848d00841c550266
Fabian Fröhlich <fafr@uni-bremen.de>
2019-11-16 19:23:31 +0100

    DummyTest

---
+++ Tests/DummyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace FabianFroehlich\Core\Api\Tests;
+
+
+use FabianFroehlich\Core\TestUtil\UnitTestCase;
+
+class DummyTest extends UnitTestCase
+{
+
+    /**
+     * @test
+     */
+    public function sample() {
+        $this->assertTrue(true);
+    }
+
+}
